6bf54e5838115844ed4582ea2492e8304434a543
ahmed-bader <ahmed.bader.buqaileh@gmail.com>
2025-05-28 22:50:57 +0300

    allowed Vercel frontend URL

--- backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/config/WebConfig.java
+++ backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/config/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -25,5 +24,4 @@ public class WebConfig {
         };
     }
 }
-
 
