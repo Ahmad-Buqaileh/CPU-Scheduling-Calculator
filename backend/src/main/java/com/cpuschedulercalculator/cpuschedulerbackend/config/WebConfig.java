148fc7d8f0ffcb9467be6492ed892ef2ad0999de
ahmed-bader <ahmed.bader.buqaileh@gmail.com>
2025-05-28 22:43:13 +0300

    allowed Vercel frontend URL

--- backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/config/WebConfig.java
+++ backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://cpu-scheduling-calculator.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -22,3 +25,5 @@ public class WebConfig {
         };
     }
 }
+
+
