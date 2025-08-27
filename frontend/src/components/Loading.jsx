fa0b52a2c0d89e06d80975b33aab53d83e2860c4
ahmed-bader <ahmed.bader.buqaileh@gmail.com>
2025-08-27 05:45:42 +0300

    Added Loading when calculating

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+
+const Loading = () => {
+    return (
+        <div className="absolute flex items-center justify-center w-full h-full bg-black/50 z-10">
+            <div className="w-25 h-25 border-white border-4 rounded-full border-t-transparent animate-spin">
+            </div>
+        </div>
+    )
+}
+
+export default Loading
