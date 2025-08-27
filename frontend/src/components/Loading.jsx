53080d9da77d5985b298e3571ee1af87664e2096
ahmed-bader <ahmed.bader.buqaileh@gmail.com>
2025-08-27 06:00:20 +0300

    Fixed an issue where the loading does not cover the full page

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,7 +1,7 @@
 
 const Loading = () => {
     return (
-        <div className="absolute flex items-center justify-center w-full h-full bg-black/50 z-10">
+        <div className="fixed flex items-center justify-center inset-0 bg-black/50 z-10">
             <div className="w-25 h-25 border-white border-4 rounded-full border-t-transparent animate-spin">
             </div>
         </div>
