2affd1856acc6aad05825b50dd469bac4b1da7cb
ahmed-bader <ahmed.bader.buqaileh@gmail.com>
2025-08-30 18:46:02 +0300

    moved algo. package into service

---
+++ backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/service/algorthims/FcfsAlgorithm.java
@@ -0,0 +1,45 @@
+package com.cpuschedulercalculator.cpuschedulerbackend.service.algorthims;
+
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.GanttChartEntry;
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.ProcessDTO;
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.ScheduleResponse;
+import com.cpuschedulercalculator.cpuschedulerbackend.utility.SchedulingUtils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class FcfsAlgorithm implements AlgorithmStrategy {
+
+    @Override
+    public ScheduleResponse calculate(List<ProcessDTO> processes, Integer quantum) {
+
+        processes.sort(Comparator.comparingInt(ProcessDTO::getArrivalTime));
+
+        int currentTime = 0;
+        int totalWait = 0;
+        int totalTurnAround = 0;
+        List<GanttChartEntry> ganttChart = new ArrayList<>();
+
+        for (ProcessDTO process : processes) {
+            if (process.getArrivalTime() > currentTime) {
+                int idleStart = currentTime;
+                int idleEnd = process.getArrivalTime();
+                ganttChart.add(new GanttChartEntry(idleStart, 0, idleEnd));
+                currentTime = idleEnd;
+            }
+
+            currentTime = SchedulingUtils.calculateProcessInfo(process,currentTime,ganttChart);
+            totalWait += process.getWaitingTime();
+            totalTurnAround += process.getTurnaroundTime();
+        }
+
+        return new ScheduleResponse(
+                processes,
+                (double) totalWait / processes.size(),
+                (double) totalTurnAround / processes.size(),
+                ganttChart
+
+        );
+    }
+}
---
+++ backend/src/main/java/com/cpuschedulercalculator/cpuschedulerbackend/service/algorthims/PrioritySchedulingAlgorithm.java
@@ -0,0 +1,58 @@
+package com.cpuschedulercalculator.cpuschedulerbackend.service.algorthims;
+
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.GanttChartEntry;
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.ProcessDTO;
+import com.cpuschedulercalculator.cpuschedulerbackend.dto.ScheduleResponse;
+import com.cpuschedulercalculator.cpuschedulerbackend.utility.SchedulingUtils;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class PrioritySchedulingAlgorithm implements AlgorithmStrategy {
+
+    @Override
+    public ScheduleResponse calculate(List<ProcessDTO> processes, Integer quantum) {
+
+        int currentTime = 0;
+        int totalWait = 0;
+        int totalTurnAround = 0;
+
+        List<ProcessDTO> temp = new ArrayList<>(processes);
+        List<ProcessDTO> completed = new ArrayList<>();
+        List<GanttChartEntry> ganttChart = new ArrayList<>();
+
+        while (!temp.isEmpty()) {
+            int finalCurrentTime = currentTime;
+            List<ProcessDTO> arrivedProcesses = temp.stream()
+                    .filter(process -> process.getArrivalTime() <= finalCurrentTime)
+                    .sorted(Comparator.comparing(ProcessDTO::getPriority))
+                    .toList();
+
+            if (arrivedProcesses.isEmpty()) {
+                if (!ganttChart.isEmpty() && ganttChart.getLast().getPid() == 0) {
+                    GanttChartEntry previousGanttChart = ganttChart.removeLast();
+                    ganttChart.add(new GanttChartEntry(previousGanttChart.getStart(), 0, currentTime + 1));
+                } else {
+                    ganttChart.add(new GanttChartEntry(currentTime, 0, currentTime + 1));
+                }
+                currentTime++;
+                continue;
+            }
+            ProcessDTO process = arrivedProcesses.getFirst();
+
+            currentTime = SchedulingUtils.calculateProcessInfo(process, currentTime, ganttChart);
+            totalWait += process.getWaitingTime();
+            totalTurnAround += process.getTurnaroundTime();
+
+            completed.add(process);
+            temp.remove(process);
+        }
+        return new ScheduleResponse(
+                completed,
+                (double) totalWait / completed.size(),
+                (double) totalTurnAround / completed.size(),
+                ganttChart
+        );
+    }
+}
